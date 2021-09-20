8cc323a8002aa6c3cf19cf5d16bca9feaa4fe189
Hamid Talebi <mr.talebihamid@hotmail.com>
2021-09-20 06:50:59 +0430

    First initial

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*=============== SHOW MENU ===============*/
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+// Previously selected topic (if user selected)
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+// We validate if the user previously chose a topic
+
+// Activate / deactivate the theme manually with the button
